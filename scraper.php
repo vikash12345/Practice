4efb75d87a4e63f998c0703eb753eda9e813af75
vikash12345 <vikash@backgroundcheck.email>
2016-08-15 12:53:04 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,6 +9,11 @@ require 'scraperwiki/simple_html_dom.php';
 <?php
 $url = 'http://202.61.43.53/cfms-hc-search/cases/search-result?CasesSearch%5BCASENAMECODE%5D=&CasesSearch%5BCASENO%5D=&CasesSearch%5BCASEYEAR%5D=&CasesSearch%5BCIRCUITCODE%5D=1&CasesSearch%5BPARTY%5D=&CasesSearch%5BMATTERCODE%5D=&CasesSearch%5BGOVT_AGENCY_CODE%5D=&CasesSearch%5BFIRNO%5D=&CasesSearch%5BFIRYEAR%5D=&CasesSearch%5BPOLICESTATIONCODE%5D=&CasesSearch%5BisPending%5D=3&page=1';
 $output = file_get_contents($url); 
-echo $output;
+
+$address = $tempdom->find("td",data-col-seq="1")->innertext
+
+
+echo $address
+
 ?>
 
